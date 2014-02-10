964d2170a90ed14ff7e68a6562c613d6abfefd6e
Martin Vidner <mvidner@suse.cz>
2014-02-10 12:56:34 +0100

    A simpler program with a CMakefile

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
+// #include <QDebug>
 #include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
+    QPushButton button("Hello, World!");
+    qDebug()    << "Button D" << &button;
+    //qWarning()  << "Button W" << button;
+    //qCritical() << "Button C" << button;
+    button.show();
     return a.exec();
 }
