293b018da5fa8cebf26845e60ae88c797161cc43
Martin Vidner <mvidner@suse.cz>
2014-06-30 16:38:46 +0200

    using dlopen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "plugin.h"
+
+typedef int (* mainfunc_t)(int, char **);
 
 int main(int argc, char *argv[])
 {
-    return plugin_main(argc, argv);
+    void * handle = dlopen("./libplugin.so", RTLD_LAZY);
+
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    dlerror();    /* Clear any existing error */
+
+    mainfunc_t plugin_main = (mainfunc_t) dlsym(handle, "_Z11plugin_mainiPPc");
+    char * error;
+    if ((error = dlerror()) != NULL)  {
+        fprintf(stderr, "%s\n", error);
+        exit(EXIT_FAILURE);
+    }
+
+    int ret = (*plugin_main)(argc, argv);
+
+    dlclose(handle);
+    // try triggering the Qt problem already
+    sleep(3); // seconds
+    return ret;
 }
