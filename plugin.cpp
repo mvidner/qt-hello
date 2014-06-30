89e68889ae7f301454c49960a81e82ea01d80630
Martin Vidner <mvidner@suse.cz>
2014-06-30 16:09:47 +0200

    moved qt code to a separate object file (no plugin yet)

---
+++ plugin.cpp
@@ -0,0 +1,38 @@
+#include <QDebug>
+#include <QApplication>
+#include <QPushButton>
+
+#include <QMessageLogContext>
+
+// http://qt-project.org/doc/qt-5.0/qtcore/qtglobal.html#qInstallMessageHandler
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        abort();
+    }
+}
+
+int plugin_main(int argc, char *argv[])
+{
+    qInstallMessageHandler(myMessageOutput);
+
+    QApplication a(argc, argv);
+    QPushButton button("Hello, World!");
+    qDebug()    << "Button D" << &button;
+    //qWarning()  << "Button W" << button;
+    //qCritical() << "Button C" << button;
+    button.show();
+    return a.exec();
+}
---
+++ plugin.h
@@ -0,0 +1,4 @@
+#ifndef PLUGIN_MAIN_H
+#define PLUGIN_MAIN_H
+int plugin_main(int argc, char *argv[]);
+#endif
