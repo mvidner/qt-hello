5e6740e68168c9e49b60eac49b7919d95f7647cc
Martin Vidner <mvidner@suse.cz>
2014-07-10 16:20:15 +0200

    Arabic example (bnc#880701, bnc#884125)
    Demonstrating truncation of Group Box label and button label in the YaST
    installation stylesheet.
      https://bugzilla.novell.com/show_bug.cgi?id=880701
      https://bugzilla.novell.com/show_bug.cgi?id=884125
    
    Use:
      wget https://raw.githubusercontent.com/yast/yast-theme/6a5ebfd5a996664950948d08e1d089383b83c969/SLE/wizard/installation.qss
      ./helloworld -stylesheet installation.qss

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QDebug>
 #include <QApplication>
 #include <QPushButton>
+#include <QGroupBox>
+#include <QRadioButton>
+#include <QHBoxLayout>
 
 #include <QMessageLogContext>
 
@@ -29,7 +32,23 @@ int main(int argc, char *argv[])
     qInstallMessageHandler(myMessageOutput);
 
     QApplication a(argc, argv);
-    QPushButton button("Hello, World!");
-    button.show();
+    a.setLayoutDirection(Qt::RightToLeft);
+
+    QGroupBox *groupBox = new QGroupBox("مرحبا");
+
+    QRadioButton *radio1 = new QRadioButton("&Radio button 1");
+    QRadioButton *radio2 = new QRadioButton("R&adio button 2");
+    QRadioButton *radio3 = new QRadioButton("Ra&dio button 3");
+    QPushButton  *button = new QPushButton("التالي");
+
+    QBoxLayout *vbox = new QHBoxLayout;
+    vbox->addWidget(radio1);
+    vbox->addWidget(radio2);
+    vbox->addWidget(radio3);
+    vbox->addStretch(1);
+    vbox->addWidget(button);
+    groupBox->setLayout(vbox);
+    groupBox->show();
+
     return a.exec();
 }
