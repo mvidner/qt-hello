66e8fe301f856f1b3a80183b1381e12074729485
Martin Vidner <mvidner@suse.cz>
2014-07-10 16:09:16 +0200

    Removed debug messages.

--- main.cpp
+++ main.cpp
@@ -30,9 +30,6 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QPushButton button("Hello, World!");
-    qDebug()    << "Button D" << &button;
-    //qWarning()  << "Button W" << button;
-    //qCritical() << "Button C" << button;
     button.show();
     return a.exec();
 }
